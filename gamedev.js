e9b9ea150c295e87eb17ca25c1ab65f5c76effe9
200067 <200067@hkis.edu.hk>
2015-12-09 10:00:32 +0800

    Changed explore right1
    - Kai

--- gamedev.js
+++ gamedev.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
 
 	$("#goRight1").click(function() {
-		$("#exploreBody").css("background-image", "url('img/exploreRight1.jpg')");
+		$("#exploreBody").css("background-image", "url('img/unnamed.jpg')");
 		$("#goRight1").hide();
 		$("#goBack").hide();
 		$("#goRight-Straight1").show();
